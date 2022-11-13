28d016aee154d3c210cf1bbd93854c64a35edd80
Przemek <p.ostrowinski@gmail.com>
2022-11-13 19:55:55 +0100

    add styles

--- src/components/MapApiKeyForm/MapApiKeyForm.tsx
+++ src/components/MapApiKeyForm/MapApiKeyForm.tsx
@@ -17,16 +17,30 @@ export const MapApiKeyForm: React.FC = () => {
   }
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>
-        Klucz Api do mapy googla
-        <input
-          name={"key" as keyof MapApiKeyDTO}
-          onChange={formik.handleChange}
-          value={formik.values.key}
-        />
-      </label>
-      <button type="submit">Dodaj klucz do localStorage</button>
-    </form>
+    <div className="flex items-center justify-center h-screen">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={formik.handleSubmit}
+      >
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Klucz Api do mapy googla
+          </label>
+          <input
+            className="bg-gray-50 mb-5 border border-gray-300 text-gray-900 w-80 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            name={"key" as keyof MapApiKeyDTO}
+            onChange={formik.handleChange}
+            value={formik.values.key}
+          />
+
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="submit"
+          >
+            Dodaj klucz do localStorage
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
--- src/components/MapFilterForm/MapFilterForm.tsx
+++ src/components/MapFilterForm/MapFilterForm.tsx
@@ -41,33 +41,50 @@ export const MapFilterForm: React.FC<Props> = ({
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label>
-        Prędność minimalna
-        <input
-          name={"speed" as keyof MapFilterDTO}
-          type="number"
-          onChange={formik.handleChange}
-          value={formik.values.speed}
-        />
-      </label>
-      <label>
-        Status
-        <select
-          name={"status" as keyof MapFilterDTO}
-          value={formik.values.status}
-          onChange={formik.handleChange}
+      <div className="grid gap-6 m-3 ml-6 mr-6  md:grid-cols-2">
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Prędność minimalna
+          </label>
+          <input
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            name={"speed" as keyof MapFilterDTO}
+            type="number"
+            onChange={formik.handleChange}
+            value={formik.values.speed}
+          />
+        </div>
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
+            Status
+          </label>
+          <select
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            name={"status" as keyof MapFilterDTO}
+            value={formik.values.status}
+            onChange={formik.handleChange}
+          >
+            {statusOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          type="submit"
         >
-          {statusOptions.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
-      </label>
-      <button type="submit">Filtruj</button>
-      <button type="button" onClick={() => formik.resetForm()}>
-        Wyszyść filtr
-      </button>
+          Filtruj
+        </button>
+        <button
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          type="button"
+          onClick={() => formik.resetForm()}
+        >
+          Wyszyść filtr
+        </button>
+      </div>
     </form>
   );
 };
